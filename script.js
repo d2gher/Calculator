54926656046b5b59f1e10763bf0ad5a0de3c1e3e
d2gher <d2gher@gmail.com>
2023-01-20 22:34:05 +0200

    Make the screen work

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let keys = document.querySelectorAll(".calculator-keys button");
+let screen = document.querySelector("#screen");
+
+keys.forEach(function(key) {
+    key.addEventListener("click", evaluate);
+})
+
+function evaluate() {
+    
+    updateScreen(this.value)
+}
+
+function updateScreen(key) {
+    if (key == "all-clear") screen.value = ""
+    else if (key == "backspace") screen.value = screen.value.slice(0, -1);
+    else screen.value += key;
+}
+
 function operate(operator, num1, num2) {
     return operator(num1, num2)
 }
